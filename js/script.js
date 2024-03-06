79948c9669e64a4f85782094e46e3d6f5c1cc3e5
Denis-Hish <denis.hish@gmail.com>
2024-03-06 10:10:59 +0100

    Determining theme and recording in LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Вступительная анимация
+// Вступительная анимация / Introductory animation
 window.addEventListener('load', function () {
    const welcomeFireText = document.querySelector('.welcome-fire-text');
    const heroSection = document.getElementById('hero-section');
@@ -13,15 +13,26 @@ window.addEventListener('load', function () {
    }, 9500);
 });
 
-// Переключение тёмной/светлой темы
-const btnThemeToggle = document.querySelector('.theme-toggle');
-btnThemeToggle.addEventListener('click', function () {
-   const currentTheme = document.body.className;
-   if (currentTheme === 'light-theme') {
-      document.body.className = 'dark-theme';
-   } else {
-      document.body.className = 'light-theme';
-   }
+// Переключение темы / Toggle theme
+document.addEventListener('DOMContentLoaded', function () {
+   const btnThemeToggle = document.querySelector('.theme-toggle');
+   const currentTheme =
+      localStorage.getItem('theme') ||
+      (window.matchMedia('(prefers-color-scheme: dark)').matches
+         ? 'dark-theme'
+         : 'light-theme');
+
+   document.body.className = currentTheme;
+   localStorage.setItem('theme', currentTheme);
+
+   btnThemeToggle.addEventListener('click', function () {
+      const currentTheme = document.body.className;
+      const newTheme =
+         currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+
+      document.body.className = newTheme;
+      localStorage.setItem('theme', newTheme);
+   });
 });
 
 // Когда загрузится весь DOM контент
