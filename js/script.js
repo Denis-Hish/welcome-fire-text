a0b6986d06dcd0b597380d61e774e384f2ef9093
Denis-Hish <denis.hish@gmail.com>
2024-06-27 10:59:42 +0200

    Small changes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 // Вступительная анимация / Introductory animation
 window.addEventListener('load', function () {
-   const intro = document.querySelector('.intro');
-   const main = document.querySelector('main');
+  const intro = document.querySelector('.intro');
+  const main = document.querySelector('main');
 
-   setTimeout(() => {
-      intro.style.opacity = 0;
-      main.style.display = 'block';
-   }, 8000);
-   setTimeout(() => {
-      intro.style.display = 'none';
-      main.style.opacity = 1;
-   }, 9500);
+  setTimeout(() => {
+    intro.style.opacity = 0;
+    main.style.display = 'block';
+  }, 8000);
+  setTimeout(() => {
+    intro.style.display = 'none';
+    main.style.opacity = 1;
+  }, 9500);
 });
 
 // Переключение темы / Toggle theme
 document.addEventListener('DOMContentLoaded', function () {
-   const btnThemeToggle = document.querySelector('.theme-toggle');
-   const currentTheme =
-      localStorage.getItem('theme') ||
-      (window.matchMedia('(prefers-color-scheme: dark)').matches
-         ? 'dark-theme'
-         : 'light-theme');
+  const btnThemeToggle = document.querySelector('.theme-toggle');
+  const currentTheme =
+    localStorage.getItem('theme') ||
+    (window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark-theme'
+      : 'light-theme');
 
-   document.body.className = currentTheme;
-   localStorage.setItem('theme', currentTheme);
+  document.body.className = currentTheme;
+  localStorage.setItem('theme', currentTheme);
 
-   btnThemeToggle.addEventListener('click', function () {
-      const currentTheme = document.body.className;
-      const newTheme =
-         currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+  btnThemeToggle.addEventListener('click', function () {
+    const currentTheme = document.body.className;
+    const newTheme =
+      currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
 
-      document.body.className = newTheme;
-      localStorage.setItem('theme', newTheme);
-   });
+    document.body.className = newTheme;
+    localStorage.setItem('theme', newTheme);
+  });
 });
 
 // Когда загрузится весь DOM контент
